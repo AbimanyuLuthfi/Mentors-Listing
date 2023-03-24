6867db0f7442eac4594f3aecc71463954015405c
AbimanyuLuthfi <abimanyu177@gmail.com>
2023-03-24 16:08:34 +0700

    update Mentors Listing Project

--- app/Views/login.php
+++ app/Views/login.php
@@ -52,7 +52,7 @@
           <div class="text-center text-lg-start mt-4 pt-2">
             <button type="submit" class="btn btn-primary btn-lg"
               style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="/auth/register"
+            <p class="small fw-bold mt-2 pt-1 mb-0">Belum Memiliki Akun? <a href="/auth/register"
                 class="link-danger">Register</a></p>
           </div>
 
